4fc5a9520c912fe6201f31a909435606d8b87e32
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2018-03-18 00:04:51 +0900

    Storeを作成

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+const initializeState = {
+	tasks: []
+};
+
+const addTask = (task) =>({
+	type: 'ADD_TASK',
+	payload: {
+		task
+	}
+});
+
+function tasksReducer(state = initializeState, action) {
+	switch (action.type) {
+		case 'ADD_TASK':
+			return {
+				...state,
+				tasks: state.tasks.concat([action.task])
+			};
+		default:
+			return state;
+	}
+}
+
+const store = createStore(tasksReducer);
