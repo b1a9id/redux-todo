85c7c6b0f696f6863d58f1d941f115468926ee2c
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2018-03-18 18:38:20 +0900

    Actionログを表示

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { render } from 'react-dom';
+// デフォルト設定を利用しない場合はcreateLoggerをインポートする
+import { createLogger } from 'redux-logger';
 import taskReducer from './reducers/tasks';
 import TodoApp from './containers/TodoApp';
 
-const store = createStore(taskReducer);
+// 高頻度で発生するActionをログに落とさないように例外として指定
+const loggerSetting = {
+	predicate: (getState, action) => action.type !== 'HIGH_FREQUENCY_ACTION'
+};
+// 設定を元にloggerミドルウェアを作成
+const logger = createLogger(loggerSetting);
+const store = createStore(taskReducer, applyMiddleware(logger));
 
 render(
 	<Provider store={store}>
