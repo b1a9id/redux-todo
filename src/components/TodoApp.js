96ce533403f513463540238f3dcff9de436ef2a1
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2018-03-21 22:15:36 +0900

    material-uiを使う

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,19 +1,38 @@
 import React from 'react';
+import Reboot from 'material-ui/Reboot';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+import Input from 'material-ui/Input';
+import List, { ListItem, ListItemText } from 'material-ui/List';
 
 export default function TodoApp({ task, tasks, inputTask, addTask }) {
 	return (
 		<div>
-			<input type="text" onChange={(e) => inputTask(e.target.value)} />
-			<input type="button" value="add" onClick={() => addTask(task)}/>
-			<ul>
-				{
-					tasks.map(function (item, i) {
-						return (
-							<li key={i}>{item}</li>
-						)
-					})
-				}
-			</ul>
+			<Reboot/>
+			<AppBar position="static">
+				<Toolbar>
+					<Typography type="title" color="inherit">
+						Todo
+					</Typography>
+				</Toolbar>
+			</AppBar>
+			<div style={{ padding: '16px' }}>
+				<Input onChange={(e) => inputTask(e.target.value)} />
+				<Button raised color="accent" onClick={() => addTask(task)}>add</Button>
+				<List>
+					{
+						tasks.map(function (item, i) {
+							return (
+								<ListItem key={i}>
+									<ListItemText primary={ `・${item}` } />
+								</ListItem>
+							);
+						})
+					}
+				</List>
+			</div>
 		</div>
 	);
 }
